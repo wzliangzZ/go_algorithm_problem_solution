8990735f8e730bb4587341e6a264279f8d3b8cbc
wzl722 <1656069145@qq.com>
2023-12-09 16:57:03 +0800

    "2023/12/9"

--- main.go
+++ main.go
@@ -3,20 +3,14 @@ package main
 import (
 	"bufio"
 	"fmt"
-	"math/bits"
 	"os"
-)
-
-const (
-	INT int   = 0x3f3f3f3f
-	MOD int   = 1_000_000_007
-	I64 int64 = (1<<bits.UintSize)/2 - 1
 )
 
 func solve(in *bufio.Reader, out *bufio.Writer) {
 	var n int
 	fmt.Fscan(in, &n)
 
+	
 }
 
 func main() {
