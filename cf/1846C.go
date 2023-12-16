695a1e222538d6f9140e2407082daf8a6f39bb4b
wzl722 <1656069145@qq.com>
2023-12-16 11:47:39 +0800

    2023/12/16

---
+++ cf/1846C.go
@@ -0,0 +1,67 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"sort"
+)
+
+type rank struct {
+	score  int
+	idx    int
+	pently int
+}
+type ranks []*rank
+
+func (rs ranks) Len() int {
+	return len(rs)
+}
+func (rs ranks) Swap(i, j int) {
+	rs[i], rs[j] = rs[j], rs[i]
+}
+func (rs ranks) Less(i, j int) bool {
+	if rs[i].score == rs[j].score {
+		if rs[i].pently == rs[j].pently {
+			return rs[i].idx < rs[j].idx
+		}
+		return rs[i].pently < rs[j].pently
+	} else {
+		return rs[i].score > rs[j].score
+	}
+}
+func CF1846C(in *bufio.Reader, out *bufio.Writer) {
+	var n, m, h int
+	fmt.Fscan(in, &n, &m, &h)
+	arr := make([][]int, n)
+	for i := 0; i < n; i++ {
+		arr[i] = make([]int, m)
+		for j := 0; j < m; j++ {
+			fmt.Fscan(in, &arr[i][j])
+		}
+		sort.Ints(arr[i])
+	}
+
+	calc := func(idx int, arr []int, h int, res *ranks) {
+		var sc, p, cur int
+		for i := 0; i < len(arr); i++ {
+			if cur+arr[i] > h {
+				break
+			}
+			cur += arr[i]
+			sc++
+			p += cur
+		}
+		*res = append(*res, &rank{sc, idx, p})
+	}
+	var res ranks
+	for i := 0; i < n; i++ {
+		calc(i, arr[i], h, &res)
+	}
+	sort.Sort(res)
+	for i, rk := range res {
+		if rk.idx == 0 {
+			fmt.Fprintln(out, i+1)
+			return
+		}
+	}
+}
--- main.go
+++ main.go
@@ -7,11 +7,27 @@ import (
 )
 
 func solve(in *bufio.Reader, out *bufio.Writer) {
-	var n int
-	fmt.Fscan(in, &n)
-
+	var n, k int
+	fmt.Fscan(in, &n, &k)
+	arr := make([]int64, k)
+	for i := 0; i < k; i++ {
+		fmt.Fscan(in, &arr[i])
+	}
+	var last int64 = 0x3f3f3f3f * 2
+	for i := k - 2; i >= 0; i-- {
+		if arr[i + 1] - arr[i] > last{
+			fmt.Fprintln(out, "NO")
+			return
+		}
+		last = arr[i + 1] - arr[i]
+	}
+	n -= k - 1
+	if int64(n) * last < arr[0]{
+		fmt.Fprintln(out, "NO")
+	}else{
+		fmt.Fprintln(out, "YES")
+	}
 }
-
 func main() {
 	in := bufio.NewReader(os.Stdin)
 	out := bufio.NewWriter(os.Stdout)
