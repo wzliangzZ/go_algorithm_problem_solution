05a7f60dedadfe7f4f252cf15c388031b51837c8
wzl722 <1656069145@qq.com>
2023-12-18 10:54:41 +0800

    2023/12/18

--- cf/962C.go
+++ cf/962C.go
@@ -4,10 +4,9 @@ import (
 	"bufio"
 	"fmt"
 	"math"
-	"os"
 )
 
-func solve(in *bufio.Reader, out *bufio.Writer) {
+func CF926C(in *bufio.Reader, out *bufio.Writer) {
 	var num int
 	fmt.Fscan(in, &num)
 	var arr []int
@@ -16,20 +15,20 @@ func solve(in *bufio.Reader, out *bufio.Writer) {
 		num /= 10
 	}
 	var res int = 15
-	for s := 0; s < 1 << len(arr); s++ {
+	for s := 0; s < 1<<len(arr); s++ {
 		var sum, cnt int
 		var k int = 1
 		for i := 0; i < len(arr); i++ {
-			if s >> i & 1 == 1 {
+			if s>>i&1 == 1 {
 				cnt++
 				sum += k * arr[i]
 				k *= 10
 			}
 		}
-		if k / 10 - sum > 0 || sum == 0{
+		if k/10-sum > 0 || sum == 0 {
 			continue
 		}
-		
+
 		min := func(a, b int) int {
 			if a < b {
 				return a
@@ -38,28 +37,16 @@ func solve(in *bufio.Reader, out *bufio.Writer) {
 		}
 		check := func(sum int) bool {
 			var n int = int(math.Sqrt(float64(sum)))
-			return n * n == sum
+			return n*n == sum
 		}
 		if check(sum) {
-			res = min(res, len(arr) - cnt)
+			res = min(res, len(arr)-cnt)
 		}
-		
+
 	}
 	if res == 15 {
 		fmt.Fprintln(out, -1)
 	} else {
 		fmt.Fprintln(out, res)
 	}
-}
-func main() {
-	in := bufio.NewReader(os.Stdin)
-	out := bufio.NewWriter(os.Stdout)
-	defer out.Flush()
-
-	var tot int = 1
-	//fmt.Fscan(in, &tot)
-	for i := 0; i < tot; i++ {
-		solve(in, out)
-	}
-
 }
