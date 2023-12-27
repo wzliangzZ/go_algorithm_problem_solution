ab248115aed889cd8b96a1307e418cea00ca4708
wzl722 <1656069145@qq.com>
2023-12-27 23:40:15 +0800

    2023/12/27

--- main.go
+++ main.go
@@ -4,42 +4,12 @@ import (
 	"bufio"
 	"fmt"
 	"os"
-	"sort"
 )
 
 func solve(in *bufio.Reader, out *bufio.Writer) {
 	var n int
 	fmt.Fscan(in, &n)
-	arr := make([]int, n)
-	cnt := make(map[int]int)
-	for i := 0; i < n; i++ {
-		fmt.Fscan(in, &arr[i])
-		cnt[arr[i]]++
-	}
-	var t []int
-	for k, _ := range cnt {
-		t = append(t, k)
-	}
-	sort.Ints(t)
-	C := func(n, k int) int {
-		var res, b int = 1, 1
-		for i := 0; i < k; i++ {
-			res *= n - i
-			b *= (i + 1)
-		}
-		return res / b
-	}
-	var k int = 3
-	var res int = 1
-	for i := 0; i < 3 && k > 0; i++ {
-		if cnt[t[i]] >= k {
-			res *= C(cnt[t[i]], k)
-			k = 0
-		}else{
-			k -= cnt[t[i]]
-		}
-	}
-	fmt.Fprintln(out, res)
+
 }
 
 func main() {
