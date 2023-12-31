7759c14b20b871dfff218d6af32cd8c09920b227
wzl722 <1656069145@qq.com>
2024-01-01 00:07:36 +0800

    2024/1/1

---
+++ luogu/P1757.go
@@ -0,0 +1,47 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	//"os"
+)
+
+func P1757(in *bufio.Reader, out *bufio.Writer) {
+	var n, m int
+	fmt.Fscan(in, &m, &n)
+	v := make([][]int, 101)
+	w := make([][]int, 101)
+	f := make([]int, m+1)
+	for i := 0; i < n; i++ {
+		var a, b, c int
+		fmt.Fscan(in, &a, &b, &c)
+		w[c] = append(w[c], a)
+		v[c] = append(v[c], b)
+	}
+	// 现枚举顺序： 组别、体积、组中物品
+	for k := 1; k <= 100; k++ {
+		for j := m; j > 0; j-- {
+			for i := 0; i < len(v[k]); i++ {
+				if j >= w[k][i] {
+					f[j] = max(f[j], f[j-w[k][i]]+v[k][i])
+				}
+			}
+		}
+	}
+	fmt.Fprintln(out, f[m])
+}
+
+/*
+func main() {
+	in := bufio.NewReader(os.Stdin)
+	out := bufio.NewWriter(os.Stdout)
+	defer out.Flush()
+
+	var tot int = 1
+	//fmt.Fscan(in, &tot)
+
+	for i := 0; i < tot; i++ {
+		P1757(in, out)
+	}
+}
+*/
--- main.go
+++ main.go
@@ -8,32 +8,8 @@ import (
 
 func solve(in *bufio.Reader, out *bufio.Writer) {
 	var n, m int
-	fmt.Fscan(in, &n, &m)
-	var v, w []int
-	f := make([]int, m + 1)
-	split := func(a, b, c int) {
-		var k int = 1
-		for k < c {
-			v = append(v, a * k)
-			w = append(w, b * k)
-			c -= k
-			k <<= 1
-		}
-		v = append(v, a * c)
-		w = append(w, b * c)
-	}
+	fmt.Fscan(in, &m, &n)
 
-	for i := 0; i < n; i++ {
-		var a, b, c int
-		fmt.Fscan(in, &a, &b, &c)
-		split(a, b, c)
-	}
-	for i := 0; i < len(v); i++ {
-		for j := m; j >= w[i]; j-- {
-			f[j] = max(f[j], f[j - w[i]] + v[i])
-		}
-	}
-	fmt.Fprintln(out, f[m])
 }
 
 func main() {
@@ -42,7 +18,7 @@ func main() {
 	defer out.Flush()
 
 	var tot int = 1
-	fmt.Fscan(in, &tot)
+	//fmt.Fscan(in, &tot)
 
 	for i := 0; i < tot; i++ {
 		solve(in, out)
