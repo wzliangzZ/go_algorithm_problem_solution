4ad9b71d7953e0c232778f16e5551f71dd12cccf
wzl722 <1656069145@qq.com>
2024-01-14 16:48:09 +0800

    updat DP

--- main.go
+++ main.go
@@ -1,22 +1,21 @@
 package main
- 
+
 import (
 	"bufio"
 	"fmt"
 	"os"
 )
- 
+
 func solve(in *bufio.Reader, out *bufio.Writer) {
 	var n int
 	fmt.Fscan(in, &n)
-
 }
- 
+
 func main() {
 	in := bufio.NewReader(os.Stdin)
 	out := bufio.NewWriter(os.Stdout)
 	defer out.Flush()
- 
+
 	var tot int = 1
 	fmt.Fscan(in, &tot)
 
