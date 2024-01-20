336e3cdfab0bf6b9ad9dfde7910350278c2a2e2d
wzl722 <1656069145@qq.com>
2024-01-20 21:30:59 +0800

    add ArrayDeque

---
+++ src/DataStruct/ArrayDeqye.go
@@ -0,0 +1,100 @@
+package DataStruct
+
+type Element struct {
+	first, second int
+}
+
+/* 基于环形数组实现的双向队列 */
+type ArrayDeque struct {
+	elements    []Element // 用于存储双向队列元素的数组
+	front       int       // 队首指针，指向队首元素
+	queSize     int       // 双向队列长度
+	queCapacity int       // 队列容量（即最大容纳元素数量）
+}
+
+/* 初始化队列 */
+func NewArrayDeque(queCapacity int) *ArrayDeque {
+	return &ArrayDeque{
+		elements:    make([]Element, queCapacity),
+		queCapacity: queCapacity,
+		front:       0,
+		queSize:     0,
+	}
+}
+
+/* 获取双向队列的长度 */
+func (q *ArrayDeque) Size() int {
+	return q.queSize
+}
+
+/* 判断双向队列是否为空 */
+func (q *ArrayDeque) IsEmpty() bool {
+	return q.queSize == 0
+}
+
+/* 计算环形数组索引 */
+func (q *ArrayDeque) Index(i int) int {
+	// 通过取余操作实现数组首尾相连
+	// 当 i 越过数组尾部后，回到头部
+	// 当 i 越过数组头部后，回到尾部
+	return (i + q.queCapacity) % q.queCapacity
+}
+
+/* 队首入队 */
+func (q *ArrayDeque) PushFront(ele Element) {
+	if q.queSize == q.queCapacity {
+		panic("双向队列已满")
+	}
+	// 队首指针向左移动一位
+	// 通过取余操作实现 front 越过数组头部后回到尾部
+	q.front = q.Index(q.front - 1)
+	// 将 Element 添加至队首
+	q.elements[q.front] = ele
+	q.queSize++
+}
+
+/* 队尾入队 */
+func (q *ArrayDeque) PushBack(ele Element) {
+	if q.queSize == q.queCapacity {
+		panic("双向队列已满")
+	}
+	// 计算队尾指针，指向队尾索引 + 1
+	rear := q.Index(q.front + q.queSize)
+	// 将 Element 添加至队首
+	q.elements[rear] = ele
+	q.queSize++
+}
+
+/* 队首出队 */
+func (q *ArrayDeque) PopFront() any {
+	ele := q.Front()
+	// 队首指针向后移动一位
+	q.front = q.Index(q.front + 1)
+	q.queSize--
+	return ele
+}
+
+/* 队尾出队 */
+func (q *ArrayDeque) PopBcck() any {
+	ele := q.Back()
+	q.queSize--
+	return ele
+}
+
+/* 访问队首元素 */
+func (q *ArrayDeque) Front() any {
+	if q.IsEmpty() {
+		return nil
+	}
+	return q.elements[q.front]
+}
+
+/* 访问队尾元素 */
+func (q *ArrayDeque) Back() any {
+	if q.IsEmpty() {
+		return nil
+	}
+	// 计算尾元素索引
+	last := q.Index(q.front + q.queSize - 1)
+	return q.elements[last]
+}
