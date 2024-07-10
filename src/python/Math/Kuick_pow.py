79d4e131eaa4ca54db4d381d7229f4c052012ee3
wzl722 <1656069145@qq.com>
2024-07-10 20:19:26 +0800

    update

--- src/python/Math/Kuick_pow.py
+++
@@ -1,9 +0,0 @@
-#快速幂
-def ksm(m: int, k: int, p: int) -> int:
-    res, t = 1 % p, m
-    while k:
-        if k & 1: res = res * t % p
-        t = t * t % p
-        k >>= 1
-    return res
-
--- src/python/Math/Quick_pow.py
+++
@@ -1,8 +0,0 @@
-#快速幂
-def ksm(m: int, k: int, p: int) -> int:
-    res = 1 % p
-    while k:
-        if k & 1: res = res * m % p
-        m = m * m % p
-        k >>= 1
-    return res
